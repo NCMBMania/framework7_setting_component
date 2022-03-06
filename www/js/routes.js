8e687a977016b39c3a92968a9cfe9a3f775e8a9e
Atsushi <atsushi@moongift.jp>
2022-03-06 13:35:02 +0900

    Init

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,5 +1,31 @@
+const beforeEnter = async ({resolve, reject, router}) => {
+	// 現在ログインしているユーザー情報を取得
+	const currentUser = ncmb.User.getCurrentUser();
+	// 認証していない場合はnullになる
+	if (!currentUser) {
+		// rejectを実行
+		reject();
+		// 認証画面に遷移
+		router.navigate('/auth');
+		// 処理完了
+		return;
+	}
+	try {
+		// データストアにダミーアクセス
+		await ncmb.DataStore('Hello').fetch();
+		// アクセスできればresolveを実行（ /settingsを表示）
+		resolve();
+	} catch (e) {
+		// 駄目だった場合は認証情報を削除
+		localStorage.removeItem("NCMB/"+ncmb.apikey+"/currentUser");
+		// rejectを実行
+		reject();
+		// 認証画面に遷移
+		router.navigate('/auth');
+	}
+};
 
-var routes = [
+const routes = [
   {
     path: '/',
     url: './index.html',
@@ -7,6 +33,11 @@ var routes = [
   {
     path: '/auth',
     componentUrl: './pages/auth.html',
+  },
+  {
+    path: '/settings',
+    componentUrl: './pages/settings.html',
+    beforeEnter,
   },
   {
     path: '(.*)',
